2000ddfa9db7eae6c34152b30ed7f3564bcafb1a
zhuchenhao <zhuchenhao@kezaihui.com>
2017-08-30 20:22:15 +0800

    add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import NotFound from '@/components/404';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello,
+    },
+    {
+      path: '*',
+      name: '404',
+      component: NotFound,
     },
   ],
 });
