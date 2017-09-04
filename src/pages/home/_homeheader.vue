f3b77de5fd45e8a63985fa4fa4cedf02a5016107
zhuchenhao <zhuchenhao@kezaihui.com>
2017-09-04 21:31:14 +0800

    add new homeheader

---
+++ src/pages/home/_homeheader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>topics</h1>
+  </header>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+header {
+  position: fixed;
+  top: 0;
+  background-color: grey;
+  width: 100%;
+}
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,11 +1,17 @@
 <template lang="html">
   <div class="">
+    <home-header></home-header>
     <h1>Home</h1>
   </div>
 </template>
 
 <script>
+import HomeHeader from './_homeheader';
+
 export default {
+  components: {
+    HomeHeader,
+  },
 };
 </script>
 
