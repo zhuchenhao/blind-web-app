69ba04258318bc6505935996005f388a9263e9e7
zhuchenhao <zhuchenhao@kezaihui.com>
2017-09-07 21:35:51 +0800

    add posts

---
+++ src/pages/home/_post.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="posts">
+    <div class="card" v-for="i in 9">
+      <div class="card__content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+  .posts {
+    background-color: #ddd;
+    height: 100%;
+  }
+  .card {
+    margin: 0;
+    border-radius: 0px;
+    margin-bottom: 8px;
+  }
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,20 +2,26 @@
   <div class="home">
     <home-header></home-header>
     <topics></topics>
+    <post></post>
   </div>
 </template>
 
 <script>
 import HomeHeader from './_homeheader';
 import topics from './_topics';
+import post from './_post';
 
 export default {
   components: {
     HomeHeader,
     topics,
+    post,
   },
 };
 </script>
 
-<style lang="css">
+<style scoped>
+.home {
+  height: 100%;
+}
 </style>
