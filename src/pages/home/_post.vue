d0e7dd3031e2de09d6c5d94f2caa0d5b68b1033c
zhuchenhao <zhuchenhao@kezaihui.com>
2017-09-09 15:07:02 +0800

    add card tags

--- src/pages/home/_post.vue
+++ src/pages/home/_post.vue
@@ -1,26 +1,49 @@
 <template lang="html">
   <div class="posts">
-    <div class="card" v-for="i in 9">
+    <div class="card" v-for= "post in posts">
       <div class="card__content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        <router-link
+          class="card__tag"
+          v-for= "topic in post.topics"
+          :to="{ name: 'topics-detail', params: {id: topic.id} }"
+        >
+          {{topic.name}}
+        </router-link>
+        <p>{{post.title}}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import posts from './posts.json';
+
 export default {
+  data() {
+    return {
+      posts,
+    };
+  },
 };
 </script>
 
 <style scoped>
-  .posts {
-    background-color: #ddd;
-    height: 100%;
-  }
-  .card {
-    margin: 0;
-    border-radius: 0px;
-    margin-bottom: 8px;
-  }
+.posts {
+  background-color: #ddd;
+  height: 100%;
+}
+.card {
+  margin: 0;
+  border-radius: 0px;
+  margin-bottom: 8px;
+}
+.card__tag {
+  display: inline-block;
+  padding: 4px 20px;
+  background-color: #ddd;
+  color: #666;
+  margin-right: 8px;
+  text-decoration: none;
+  border-radius: 14px;
+}
 </style>
