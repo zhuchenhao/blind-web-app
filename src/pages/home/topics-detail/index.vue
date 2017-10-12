fff1383eb498606d273723484f58051319a0f0f9
zhuchenhao <zhuchenhao@kezaihui.com>
2017-10-12 19:55:37 +0800

    add topic details page

--- src/pages/home/topics-detail/index.vue
+++ src/pages/home/topics-detail/index.vue
@@ -1,15 +1,47 @@
 <template lang="html">
   <div class="">
-    <h1>topic-detail</h1>
+    <div class="topic-header">
+      <span class="topic-back" @click="goBack">
+        <i class="ion-ios-arrow-back"></i>
+      </span>
+      <span style="line-height:65px;">
+        Career
+      </span>
+    </div>
+    <post></post>
   </div>
 </template>
 
 <script>
+import post from '../_post';
+
 export default {
   created() {
+  },
+  methods: {
+    goBack() {
+      window.history.back();
+    },
+  },
+  components: {
+    post,
   },
 };
 </script>
 
 <style lang="css">
+.topic-header {
+  background-color: #357BE8;
+  height: 65px;
+  text-align: center;
+}
+.topic-back {
+  position: fixed;
+  left: 0;
+  top: 0;
+  padding-left: 14px;
+  padding-top: 18px;
+  cursor: pointer;
+  height: 65px;
+}
 </style>
