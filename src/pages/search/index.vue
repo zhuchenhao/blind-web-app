0ab40ca0c165944fc0356ee2c90d57d29028c386
zhuchenhao <zhuchenhao@kezaihui.com>
2017-10-12 21:33:44 +0800

    search input

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -1,13 +1,43 @@
 <template lang="html">
   <div class="">
-    <h1>Search</h1>
+    <div class="search-box">
+      <input
+        type="search"
+        value=""
+        placeholder="Keywords #hashtag"
+        class="search-input"
+        ref="searchInput"
+        :class="{'focused':focused}"
+        @focus="focused=true"
+        @blur="focused=false"
+      >
+    </div>
+    <p>{{focused}}</p>
   </div>
 </template>
 
 <script>
 export default {
+  mounted() {
+    this.$refs.searchInput.focus();
+  },
+  data() {
+    return {
+      focused: false,
+    };
+  },
 };
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
+.search-box {
+  margin-top: 10px;
+  width: 100%;
+}
+.search-input {
+  width: 90%;
+}
+.focused {
+  width: 75%;
+}
 </style>
