b46e6e22d9de5314a375ab4931d246cf4c379bba
zhuchenhao <zhuchenhao@kezaihui.com>
2017-11-01 21:36:54 +0800

    add subject and text area

--- src/pages/edit/index.vue
+++ src/pages/edit/index.vue
@@ -1,15 +1,44 @@
 <template lang="html">
   <div>
     <header class="edit-header">
-      <span>Cancel</span>
+      <span style="color: #3468e7" @click="goBack">Cancel</span>
       <span>Topics</span>
       <span>Post</span>
     </header>
+    <div class="select-topics">
+      <select class="select-input">
+        <option>
+          Choose a topic
+        </option>
+        <option>Women in tech</option>
+        <option>Facebook</option>
+      </select>
+    </div>
+    <div class="content">
+      <div class="content-subject">
+        <input type="text"
+        class="text-input text-input--underbar"
+        placeholder="Subject"
+        ref="subject">
+      </div>
+      <div class="content-text">
+        <textarea class="textarea textarea--transparent" rows="3" placeholder="Keep it relevant. If the "></textarea>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
+  methods: {
+    goBack() {
+      window.history.back();
+    },
+  },
+  mounted() {
+    this.$refs.subject.focus();
+  },
 };
 </script>
 
@@ -23,6 +52,30 @@ export default {
     font-size: 16px;
     padding-left: 20px;
     padding-right: 20px;
+    border-bottom: 1px solid #eee;
+  }
+  .select-topics {
+    padding-left: 20px;
+    padding-right: 20px;
+    border-bottom: 1px solid #eee;
+  }
+  .select-input {
+    width: 100%;
+    height: 50px;
+  }
+  .content {
+    padding: 0 20px;
+  }
+  .text-input {
+    width:100%;
+    height: 50px;
+  }
+  .text-input--underbar {
+    border-color: #eee;
+  }
+  .textarea {
+    width: 100%;
+    height: 140px;
     border-bottom: 1px solid #eee;
   }
 </style>
