87a32e084c0d5445bc979efe4fd6096b96bb4240
zhuchenhao <zhuchenhao@kezaihui.com>
2017-11-01 20:42:11 +0800

    add edit header

--- src/pages/edit/index.vue
+++ src/pages/edit/index.vue
@@ -1,6 +1,10 @@
 <template lang="html">
-  <div class="">
-    <h1>Edit</h1>
+  <div>
+    <header class="edit-header">
+      <span>Cancel</span>
+      <span>Topics</span>
+      <span>Post</span>
+    </header>
   </div>
 </template>
 
@@ -9,5 +13,16 @@ export default {
 };
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
+  .edit-header {
+    background-color: #fefefe;
+    display: flex;
+    justify-content: space-between;
+    height: 65.156px;
+    line-height: 65.156px;
+    font-size: 16px;
+    padding-left: 20px;
+    padding-right: 20px;
+    border-bottom: 1px solid #eee;
+  }
 </style>
